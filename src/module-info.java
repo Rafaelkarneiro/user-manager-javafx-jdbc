726033245d0320e9160e1be1593d3e28a358983c
Rafael Campanin Carneiro <rafaelcampanincarneiro@gmail.com>
2024-02-05 16:12:10 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module user_manager_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	opens application to javafx.graphics, javafx.fxml;
 	exports gui to javafx.fxml;
 
