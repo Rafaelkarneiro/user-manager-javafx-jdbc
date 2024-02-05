5c9f355e313365855ab9aeeb0f684c734e74dca2
Rafael Campanin Carneiro <rafaelcampanincarneiro@gmail.com>
2024-02-05 19:33:20 -0300

    Adding database access

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module user_manager_javafx_jdbc {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
 	opens application to javafx.graphics, javafx.fxml;
